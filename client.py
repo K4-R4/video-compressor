2fc5ccfb690275cc58e92b920fc119bcbd9e7f2e
K4R4 <mabdman512@gmail.com>
2023-12-16 14:56:45 +0900

    fix

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import mimetypes
+import os.path
 import socket
+import struct
 import sys
 
 
@@ -12,20 +14,38 @@ class FileValidator:
 
 class Client:
     buffer_size = 1400
+    max_file_size = 2 ** 32
+    header_size = 32
+    response_size = 16
 
-    def __init__(self, host, port):
+    def __init__(self, host: str, port: int):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = host
         self.port = port
 
-    def run(self, file_name):
+    def run(self, file_name: str):
         self.sock.connect((self.host, self.port))
         if not FileValidator.is_mp4(file_name):
             print('The file must be mp4 format')
+            self.sock.close()
             return
-        self.send_message(file_name)
+        self.send_header(file_name)
+        self.send_body(file_name)
 
-    def send_message(self, file_name):
+    # ファイルサイズを32バイトで送信する
+    # 2^32 = 4GBまでしか送信できない
+    def send_header(self, file_name: str):
+        file_size = os.path.getsize(file_name)
+        print(f'File size: {file_size}')
+        if file_size > Client.max_file_size:
+            print('File size is too large!')
+            self.sock.close()
+            return
+        # ファイルサイズを32バイトにするために空白で埋める
+        packed_data = struct.pack('32s', file_size.to_bytes(32, 'big'))
+        self.sock.send(packed_data)
+
+    def send_body(self, file_name: str):
         with open(file_name, 'rb') as f:
             try:
                 # self.buffer_size分ずつファイルを読み込んで送信する
@@ -34,10 +54,19 @@ class Client:
                     if data:
                         self.sock.send(data)
                     else:
+                        print('File has been sent!')
+                        resp = self.receive_status()
+                        print(f'Response from server: {resp}')
                         break
             # socketが例外を発生させたら接続を切る
             except socket.error:
                 self.sock.close()
+
+    def receive_status(self) -> int:
+        # サーバーからのレスポンスを受け取る
+        data = self.sock.recv(Client.response_size)
+        status_code_bytes, = struct.unpack('16s', data)
+        return int.from_bytes(status_code_bytes, 'big')
 
 
 def main():
@@ -46,7 +75,13 @@ def main():
     if len(sys.argv) != 2:
         print('Usage: python client.py [file_name]')
         sys.exit(1)
-    client.run(sys.argv[1])
+
+    file_name = sys.argv[1]
+    # ファイルが存在するか確認する
+    if not os.path.exists(file_name):
+        print(f'{sys.argv[1]} does not exist!')
+        sys.exit(1)
+    client.run(file_name)
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,14 +1,19 @@
 import socket
+import struct
 import threading
 
 
 class Server:
     buffer_size = 1400
+    header_size = 32
+    response_size = 16
 
-    def __init__(self, host, port):
+    def __init__(self, host: str, port: int):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = host
         self.port = port
+        # プログラム終了時にソケットを閉じる
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
 
         self.sock.listen(5)
@@ -23,26 +28,57 @@ class Server:
                 client, address = self.sock.accept()
                 print(f'Connection from {address} has been established!')
                 client.settimeout(60)
-                threading.Thread(target=self.listen_to_client, args=(client, address)).start()
+                threading.Thread(target=Server.listen_to_client, args=(client, address)).start()
         except KeyboardInterrupt as e:
             print(e)
         finally:
             self.sock.close()
 
     # クライアントからのメッセージを待ち受ける
-    def listen_to_client(self, client, address):
+    @staticmethod
+    def listen_to_client(client: socket.socket, address: tuple):
+        file_size = Server.receive_header(client)
+        if file_size == 0:
+            return
+        Server.receive_body(client, file_size)
+
+    @staticmethod
+    def receive_header(client: socket.socket) -> int:
+        # 32バイトのヘッダーを受信する
+        header = client.recv(32)
+        try:
+            file_size_bytes, = struct.unpack('32s', header)
+            file_size = int.from_bytes(file_size_bytes, 'big')
+            print('File size: ', file_size)
+            return file_size
+        except struct.error as e:
+            print(e)
+            client.close()
+            return 0
+
+    @staticmethod
+    def receive_body(client: socket.socket, file_size: int):
         # クライアントから送られたデータをファイルに保存する
-        with open(f'{address}.mp4', 'wb') as f:
-            while True:
-                try:
-                    data = client.recv(self.buffer_size)
-                    if data:
-                        f.write(data)
-                    else:
-                        break
-                # socketが例外を発生させたら接続を切る
-                except socket.error:
-                    client.close()
+        with open('dest/dest.mp4', 'wb') as f:
+            try:
+                while file_size > 0:
+                    data = client.recv(Server.buffer_size)
+                    file_size -= len(data)
+                # 受信が終わったらクライアントに終了を通知する
+                print('File has been received!')
+                Server.respond(client, 200)
+                client.close()
+            # socketが例外を発生させたら接続を切る
+            except socket.error as e:
+                print(e)
+                client.close()
+                return
+
+    @staticmethod
+    def respond(client: socket.socket, data: int):
+        # クライアントにデータを送信する
+        packed_data = struct.pack('16s', data.to_bytes(16, 'big'))
+        client.send(packed_data)
 
 
 def main():
